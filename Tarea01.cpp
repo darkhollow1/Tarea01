819321ea14c9dc44d59c488f1f25ad5a34c85cd8
David <daarubiope@unal.edu.co>
2022-03-22 01:56:44 -0500

    guardando la ultima modificacion

--- Tarea01.cpp
+++ Tarea01.cpp
@@ -10,7 +10,9 @@ int main(void)
     for( n = 11; n <= NMAX ; n=n+2){ // sucesion de numeros impares n entre 10 y 100
     result = result +  1.0/n; // serie de la suma de los inversos de numeros impares entre 10 y 100
     i=i+1;
-        std::cout   << i << "\t" << result << "\n";
+        std::cout   << i << "\t" << result << "\n"; 
     }
-    return 0;
+    return 0; 
+
+    //modificando archivo para el git
 }
